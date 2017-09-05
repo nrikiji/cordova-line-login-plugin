275eb9de2fe7e75af7a7b95d696a99d87c58885d
nrikiji <nrikiji@gmail.com>
2017-09-05 09:47:44 +0900

    android対応

---
+++ src/android/LineLogin.java
@@ -0,0 +1,59 @@
+package plugin.line;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.linecorp.linesdk.LineApiResponseCode;
+import com.linecorp.linesdk.LineProfile;
+import com.linecorp.linesdk.auth.LineLoginApi;
+import com.linecorp.linesdk.auth.LineLoginResult;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LineLogin extends CordovaPlugin {
+
+    String channelId;
+    CallbackContext callbackContext;
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("initialize")) {
+            JSONObject params = data.getJSONObject(0);
+            channelId = params.get("channel_id").toString();
+            return true;
+        } else if (action.equals("login")) {
+            Context context = this.cordova.getActivity().getApplicationContext();
+            Intent loginIntent = LineLoginApi.getLoginIntent(context, channelId);
+            this.cordova.startActivityForResult((CordovaPlugin) this, loginIntent, 0);
+            this.callbackContext = callbackContext;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        LineLoginResult result = LineLoginApi.getLoginResultFromIntent(data);
+        JSONObject json = new JSONObject();
+        if (result.getResponseCode() == LineApiResponseCode.SUCCESS) {
+            LineProfile profile = result.getLineProfile();
+            try {
+                json.put("userId", profile.getUserId());
+                json.put("displayName", profile.getDisplayName());
+                json.put("pictureURL", profile.getPictureUrl());
+                callbackContext.success(json);
+            } catch (JSONException e) {
+                callbackContext.error(-1);
+            }
+        } else {
+            callbackContext.error(-1);
+        }
+    }
+}
