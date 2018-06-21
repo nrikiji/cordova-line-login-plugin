5eac1692ab38d9647af4d1f75c0dc67074c78b77
nrikiji <nrikiji@gmail.com>
2018-06-21 16:58:50 +0900

    Update AppDelegate+LineLogin.m

--- src/ios/AppDelegate+LineLogin.m
+++ src/ios/AppDelegate+LineLogin.m
@@ -7,13 +7,41 @@
 //
 
 #import "AppDelegate+LineLogin.h"
+#import <objc/runtime.h>
 #import <LineSDK/LineSDK.h>
 
 @implementation AppDelegate (LineLogin)
 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary *)options
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector);
+
++(void)load {
+    swizzleMethod([AppDelegate class],
+                  @selector(application:openURL:options:),
+                  @selector(line_application_options:openURL:options:));
+}
+
+- (BOOL)line_application_options: (UIApplication *)app
+                         openURL: (NSURL *)url
+                         options: (NSDictionary *)options
 {
-    return [[LineSDKLogin sharedInstance] handleOpenURL:url];
+    
+    NSRange range = [url.absoluteString rangeOfString:@"line3rdp"];
+    if (range.location != NSNotFound) {
+        return [[LineSDKLogin sharedInstance] handleOpenURL:url];
+    } else {
+        return [self line_application_options:app openURL:url options:options];
+    }
+}
+
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector) {
+    Method destinationMethod = class_getInstanceMethod(class, destinationSelector);
+    Method sourceMethod = class_getInstanceMethod(class, sourceSelector);
+    
+    if (class_addMethod(class, destinationSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod))) {
+        class_replaceMethod(class, destinationSelector, method_getImplementation(destinationMethod), method_getTypeEncoding(destinationMethod));
+    } else {
+        method_exchangeImplementations(destinationMethod, sourceMethod);
+    }
 }
 
 @end
