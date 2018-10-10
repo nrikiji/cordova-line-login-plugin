081b1b3dfb1e68d7880ccb98a3a90e8970ee45a0
nrikiji <nrikiji@gmail.com>
2018-10-11 01:44:33 +0900

    prepare for ionic-native

--- www/lineLogin.js
+++ www/lineLogin.js
@@ -7,9 +7,15 @@ var LineLogin = {
   initialize: function(param, onSuccess, onFail) {
     return exec(onSuccess, onFail, 'LineLogin', 'initialize', [param]);
   },
-  
+
+  // @TODO delete param because unnecessary
   login: function(param, onSuccess, onFail) {
     return exec(onSuccess, onFail, 'LineLogin', 'login', [param]);
+  },
+
+  // @TODO replace login
+  login2: function(onSuccess, onFail) {
+    return exec(onSuccess, onFail, 'LineLogin', 'login', []);
   },
 
   logout: function(onSuccess, onFail) {
