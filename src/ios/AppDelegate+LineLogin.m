a7c9ea2ffa6be17fab97620815b19e7a625f3154
nrikiji <nrikiji@gmail.com>
2019-01-15 20:03:21 +0900

    Update AppDelegate+LineLogin.m

--- src/ios/AppDelegate+LineLogin.m
+++ src/ios/AppDelegate+LineLogin.m
@@ -16,4 +16,30 @@
     return [[LineSDKLoginManager sharedManager] application:app open:url options:options];
 }
 
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector);
+
++(void)load {
+    swizzleMethod([AppDelegate class], @selector(application:openURL:options:), @selector(line_application_options:openURL:options:));
+}
+
+- (BOOL)line_application_options: (UIApplication *)app openURL: (NSURL *)url options: (NSDictionary *)options {
+    NSRange range = [url.absoluteString rangeOfString:@"line3rdp"];
+    if (range.location != NSNotFound) {
+        return [[LineSDKLoginManager sharedManager] application:app open:url options:options];
+    } else {
+        return [self line_application_options:app openURL:url options:options];
+    }
+}
+
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector) {
+    Method destinationMethod = class_getInstanceMethod(class, destinationSelector);
+    Method sourceMethod = class_getInstanceMethod(class, sourceSelector);
+
+    if (class_addMethod(class, destinationSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod))) {
+        class_replaceMethod(class, destinationSelector, method_getImplementation(destinationMethod), method_getTypeEncoding(destinationMethod));
+    } else {
+        method_exchangeImplementations(destinationMethod, sourceMethod);
+    }
+}
+
 @end
